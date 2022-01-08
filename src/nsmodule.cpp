f55bcf055e13d84050a82f3125e14102923ac40a
obhi <obi.de.online@gmail.com>
2022-01-08 21:43:33 +0100

    Fix test target name

--- src/nsmodule.cpp
+++ src/nsmodule.cpp
@@ -993,7 +993,7 @@ void nsmodule::write_tests(std::ostream& ofs, nsbuild const& bc) const
 {
   for (auto const& t : tests)
   {
-    ofs << fmt::format("\nadd_test(NAME {}\n  COMMAND  ./{} ", t.name, target_name);
+    ofs << fmt::format("\nadd_test(NAME {}\n  COMMAND {} ", t.name, target_name);
     ofs << fmt::format("--test=\"{}\" ", t.name);
     bool first = true;
     for (auto const& p : t.parameters)
