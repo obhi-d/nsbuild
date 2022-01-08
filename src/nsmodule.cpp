ea696f27ad635e5eb5097a7e54292e4ae93e04b4
obhi <obi.de.online@gmail.com>
2022-01-08 21:50:59 +0100

    Remove quotes

--- src/nsmodule.cpp
+++ src/nsmodule.cpp
@@ -994,7 +994,7 @@ void nsmodule::write_tests(std::ostream& ofs, nsbuild const& bc) const
   for (auto const& t : tests)
   {
     ofs << fmt::format("\nadd_test(NAME {}\n  COMMAND ${{config_rt_dir}}/bin/{} ", t.name, target_name);
-    ofs << fmt::format("--test=\"{}\" ", t.name);
+    ofs << fmt::format("--test={} ", t.name);
     bool first = true;
     for (auto const& p : t.parameters)
     {
@@ -1003,7 +1003,7 @@ void nsmodule::write_tests(std::ostream& ofs, nsbuild const& bc) const
         first = false;
         continue;
       }
-      ofs << fmt::format("--test-param-{}=\"{}\" ", p.first, p.second);
+      ofs << fmt::format("--test-param-{}={} ", p.first, p.second);
     }
     ofs << "\n  WORKING_DIRECTORY ${config_rt_dir}/bin)";
   }
